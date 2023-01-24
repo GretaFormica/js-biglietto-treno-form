b268ed1e9dd332fb259b1247d7fb9dc98202998d
GretaFormica <greta.formica@gmail.com>
2023-01-24 15:23:31 +0100

    add event listener in genera

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 //dichiarazioni costanti e variabili
 const UserName = document.getElementById('name').value;
 const Km = parseInt(document.getElementById('km').value);
-const Anni = documenti.getElementById('anni').value;
+const Anni = document.getElementById('anni').value;
 const PrezzoBase = Km * 0.21;
 let Prezzo
 
+const Genera = document.getElementById('BtnGenera');
+
 //istruzioni condizionali
 
-if(!isNaN(Km)){
+Genera.addEventListener(
+    'click',
 
-    if(Anni == "Minorenne"){
-
-        Prezzo = (PrezzoBase * 20 / 100);
-    
-    } else if(Anni == "Anziano"){
+    function() {
         
-        Prezzo = (PrezzoBase * 40 / 100);
-    
-    } else {
-    
-        Prezzo = PrezzoBase;
-    
+        if(!isNaN(Km)){
+
+            if(Anni == "Minorenne"){
+        
+                Prezzo = (PrezzoBase * 20 / 100);
+            
+            } else if(Anni == "Anziano"){
+                
+                Prezzo = (PrezzoBase * 40 / 100);
+            
+            } else {
+            
+                Prezzo = PrezzoBase;
+            
+            }
+        
+        } else{
+            alert('valore dei km non valido: inserire un numero')
+        }
+
+        //arrotondamento
+        Prezzo = Prezzo.toFixed(2);
+
+        //output
+        document.getElementById('h2').innerHTML= `Il prezzo totale per il tuo viaggio è di ${Prezzo} euro`;
+
     }
+)
 
-} else{
-    alert('valore dei km non valido: inserire un numero')
-}
-
-//arrotondamento
-Prezzo = Prezzo.toFixed(2);
-console.log(Prezzo);
-
-//output
-
-//document.getElementById('h1').innerHTML= `Il prezzo totale per il tuo viaggio è di ${Prezzo} euro`;
